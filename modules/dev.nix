5f7c6589b219b8259a7711f827dd525db5c7fbd7
Humaid Alqasimi <git@huma.id>
2026-05-08 18:31:25 +0400

    add dev
    Signed-off-by: Humaid Alqasimi <git@huma.id>

---
+++ modules/dev.nix
@@ -0,0 +1,70 @@
+{ pkgs, ... }:
+{
+  config = {
+    services.nginx = {
+      enable = true;
+      recommendedTlsSettings = true;
+      recommendedProxySettings = false;
+      recommendedOptimisation = true;
+      appendHttpConfig = proxyHeaders;
+      virtualHosts = {
+        "git.works.ae" = {
+          locations."/" = {
+            proxyPass = "http://127.0.0.1:3939";
+          };
+          extraConfig = ''
+            # allow large file uploads for lfs
+            client_max_body_size 50000M;
+          '';
+        };
+      };
+    };
+    services.postgresql = {
+      enable = true;
+      package = pkgs.postgresql_17;
+      extensions =
+        ps: with ps; [
+          postgis
+          pgvector
+        ];
+      settings = {
+        max_connections = 200;
+      };
+    };
+
+    services.forgejo = {
+      enable = true;
+      package = pkgs.forgejo; # default is lts
+      database.type = "postgres";
+      lfs.enable = true;
+      settings = {
+        DEFAULT.APP_NAME = "git.works.ae";
+        server = {
+          DOMAIN = "git.alq.ae";
+          ROOT_URL = "https://git.works.ae/";
+          HTTP_PORT = 3939;
+          SSH_PORT = 2222;
+          START_SSH_SERVER = true;
+
+          LANDING_PAGE = "explore";
+        };
+        repository = {
+          DEFAULT_PRIVATE = "private";
+          ENABLE_PUSH_CREATE_USER = true;
+          ENABLE_PUSH_CREATE_ORG = true;
+        };
+        "repository.pull-request" = {
+          DEFAULT_MERGE_STYLE = "rebase";
+        };
+        other = {
+          SHOW_FOOTER_TEMPLATE_LOAD_TIME = false;
+          SHOW_FOOTER_VERSION = false;
+        };
+        "ui.meta" = {
+          AUTHOR = "git.works.ae";
+          DESCRIPTION = "A private software forge";
+        };
+      };
+    };
+  };
+}
